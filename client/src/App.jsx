504d07ddd62f34746d732c9dd8092761a9e9474a
jullianrives7 <114496707+jullianrives7@users.noreply.github.com>
2022-12-12 16:16:01 -0600

    added api data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import List from "./List";
 import AddBtn from "./AddBtn";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
   const [items, setItems] = useState([]);
+  let ApiUrl = "https://mvp2-api.onrender.com";
 
   const contextData = {
     items,
     setItems,
+    ApiUrl,
   };
+
+  const getTasksFromApi = async () => {
+    let getData = await axios.get(`${ApiUrl}/tasks`);
+    setItems([...getData.data]);
+  };
+
+  useEffect(() => {
+    getTasksFromApi();
+  }, []);
 
   console.log("items: ", items);
 
   return (
     <listContextStates.Provider value={{ ...contextData }}>
       <div className="App">
-        Hello World!
         <List />
         <AddBtn />
       </div>
--- client/src/List.jsx
+++ client/src/List.jsx
@@ -3,16 +3,17 @@ import Item from "./Item";
 import AddBtn from "./AddBtn";
 import { useContext } from "react";
 import { listContextStates } from "./App";
+import { isAccordionItemSelected } from "react-bootstrap/esm/AccordionContext";
 
 const List = () => {
-  const { addNewRow, items } = useContext(listContextStates);
+  const { items } = useContext(listContextStates);
 
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Header>To-Do List</Card.Header>
       <ListGroup variant="flush">
         {items.map((item) => {
-          return <Item item={item} />;
+          return <Item key={item.id} item={item.task} />;
         })}
       </ListGroup>
     </Card>
